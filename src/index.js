cf6aaa80f00b45a41d3b610b2edc649f61d2f679
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2025-09-30 22:25:20 +0200

    chore: initial setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+// example of importing an image
+/*
+
+import odinImage from "./odin.png";
+   
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
+
+*/
