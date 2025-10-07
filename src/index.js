d7a36d0958fa66cd6a6637b5dd0839ae6ea9e59f
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2025-10-07 22:33:50 +0200

    feat(nav): implement tab switching between home and menu

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import "./styles.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import { createHomeModule } from "./home.js";
-import { createMenuModule} from "./menu.js";
+import { createMenuModule } from "./menu.js";
 
-// need to remove and add class for grid layout
-createHomeModule();
-// createMenuModule();
+const content = document.getElementById("content");
+const btnHome = document.getElementById("btnHome");
+const btnMenu = document.getElementById("btnMenu");
+
+btnHome.addEventListener("click", () => {
+  clearContent();
+  content.classList.remove("menu-module");
+  content.classList.add("home-module");
+  createHomeModule();
+});
+
+btnMenu.addEventListener("click", () => {
+  clearContent();
+  content.classList.remove("home-module");
+  content.classList.add("menu-module");
+  createMenuModule();
+});
+
+function clearContent() {
+  content.innerHTML = "";
+}
+
+function firstCall() {
+  content.classList.add("home-module");
+  createHomeModule();
+}
+
+firstCall();
