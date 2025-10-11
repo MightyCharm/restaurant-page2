7e5282e7f08c8254a567f82fe3680dedd1cc3433
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2025-10-11 17:00:33 +0200

    feat(footer): add contact info and update layout

--- src/footer.js
+++ src/footer.js
@@ -1,25 +1,73 @@
 function createFooter() {
+  const dataLocation = [
+    {
+      text: "The Recovery Room",
+    },
+    {
+      text: "123 Serenity Lane",
+    },
+    {
+      text: "Portland, OR 97205",
+    },
+    {
+      text: "USA",
+    },
+  ];
   const footer = document.createElement("footer");
-  const para1 = document.createElement("p");
-  const div = document.createElement("div");
-  const h3 = document.createElement("h3");
-  const para2 = document.createElement("p");
+  const header = document.createElement("header");
+  const divLocation = document.createElement("div");
+  const divContacts = document.createElement("div");
+  const h3_address = document.createElement("h3");
+  const ul_address = document.createElement("ul");
+  const h3_contacts = document.createElement("h3");
+  const ul_contacts = document.createElement("ul");
+  const li_1 = document.createElement("li");
+  const li_2 = document.createElement("li");
+  const li_3 = document.createElement("li");
+  const a = document.createElement("a");
+  a.href = "#";
+  a.target = "_blank";
 
-  para1.classList.add("sentence");
-  div.classList.add("location");
+  header.classList.add("sentence");
+  divLocation.classList.add("location");
+  divContacts.classList.add("contacts");
+
   footer.classList.add("card", "card-restaurant-footer");
 
-  para1.innerHTML =
+  header.innerHTML =
     "<span>Good</span> food, <span>good</span> vibes, <span>good</span> people — welcome home.";
-  h3.textContent = "Location";
-  para2.innerHTML =
-    "The Recovery Room<br />123 Serenity Lane<br />Portland, OR 97205<br />USA";
 
-  div.appendChild(h3);
-  div.appendChild(para2);
+  h3_address.textContent = "Location";
+  h3_contacts.textContent = "Contacts";
 
-  footer.appendChild(para1);
-  footer.appendChild(div);
+  // create list items and append to ul list
+  dataLocation.forEach((item) => {
+    const li = document.createElement("li");
+    li.textContent = item.text;
+
+    ul_address.appendChild(li);
+  });
+
+  li_1.textContent = "(555) 867-5309";
+  a.textContent = "info@recoveryroom.com";
+  li_2.appendChild(a);
+  li_3.innerHTML = `<a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+                        <a href="#" target="_blank"><i class="fab fa-facebook"></i></a>
+  `;
+
+  ul_contacts.appendChild(li_1);
+  ul_contacts.appendChild(li_2);
+  ul_contacts.appendChild(li_3);
+
+  divLocation.appendChild(h3_address);
+  divLocation.appendChild(ul_address);
+
+  divContacts.appendChild(h3_contacts);
+  divContacts.appendChild(ul_contacts);
+
+  footer.appendChild(header);
+  footer.appendChild(divLocation);
+  footer.appendChild(divContacts);
   return footer;
 }
 
